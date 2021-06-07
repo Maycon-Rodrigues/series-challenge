84d1f43bbfc21c050fc8bd08fe094097535245a3
Maycon-Rodrigues <maycon.zng@gmail.com>
2021-06-07 14:52:59 -0300

    create enum for category

---
+++ Enum/Category.cs
@@ -0,0 +1,12 @@
+namespace SeriesChallenge.Enum
+{
+    public enum Category
+    {
+        Action = 1,
+        Adventure = 2,
+        Drama = 3,
+        Romance = 4,
+        Comedy = 5,
+        
+    }
+}

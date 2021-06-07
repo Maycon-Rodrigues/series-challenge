17c628d37dbd231fafcda779d075e0f77706e12c
Maycon-Rodrigues <maycon.zng@gmail.com>
2021-06-07 14:50:18 -0300

    create abstract class for entity

---
+++ Class/BaseEntity.cs
@@ -0,0 +1,12 @@
+namespace SeriesChallenge.Class
+{
+    public abstract class BaseEntity
+    {
+        protected int Id;
+
+        public int id
+        {
+            get { return Id; }
+        }
+    }
+}
